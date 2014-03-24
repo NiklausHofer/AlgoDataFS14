3a0667f6ed58200783ff4064a5a35898da8bfc26
PeterSchwab <peter.schwab@bfh.ch>
2014-03-24 10:34:37 +0100

    remove aded to MyAVLTree

--- src/examples/MyAVLTree.java
+++ src/examples/MyAVLTree.java
@@ -5,6 +5,8 @@ package examples;
 
 import java.util.Iterator;
 
+import examples.MyTree.TNode;
+
 /**
  * @author ps
  *
@@ -54,14 +56,28 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 			left = new AVLNode();
 			right = new AVLNode();
 			left.parent = this;
-			right.parent = this;			
+			right.parent = this;
+			height = 1;
 		}
 	}
+
+	
 	
 	// istance variables:
 	private AVLNode root = new AVLNode();
 	private int size;
 	
+	private AVLNode checkAndCast(Locator<K,E> p){
+		try {
+			AVLNode n = (AVLNode) p;			
+			if (n.owner == null) throw new RuntimeException(" allready removed locator!");
+			if (n.owner != this) throw new RuntimeException(" locator belongs to another AVLTree instance");
+			return n;
+		} catch (ClassCastException e) {
+			throw new RuntimeException(" locator belongs to another container-type ");
+		}
+	}
+
 	
 	
 	
@@ -75,8 +91,20 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 	}
 
 	public Locator<K, E> find(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		// returns the leftmost occurence of
+		// 'key' or null
+		AVLNode n = root;
+		AVLNode ret = null;
+		while (! n.isExternal()){
+			int comp = key.compareTo(n.key);
+			if (comp==0){
+				ret = n;
+				n = n.left;
+			}
+			else if(comp<0) n = n.left;
+			else n = n.right;
+		}
+		return ret;
 	}
 
 	/* (non-Javadoc)
@@ -96,7 +124,7 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 		AVLNode n = root;
 		while ( ! n.isExternal()){
 			int comp = key.compareTo(n.key);
-			if (comp < 0) n=n.left;
+			if (comp <= 0) n=n.left;
 			else n=n.right;
 		}
 		n.expand(key, o);
@@ -109,9 +137,38 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 	 */
 	@Override
 	public void remove(Locator<K, E> loc) {
-		// TODO Auto-generated method stub
-
+		AVLNode n = checkAndCast(loc);
+		if (n.left.isExternal() || n.right.isExternal()){
+			removeAboveExternal(n);
+		}
+		// find the most right node in the left subtree
+		AVLNode p = n.left;
+		while (! p.right.isExternal()) p=p.right;
+		removeAboveExternal(p);
+		// now we replace n by p
+		p.parent = n.parent;
+		p.right = n.right;
+		p.left = n.left;
+		if (n.isLeftChild()) n.parent.left=p;
+		else if (n.isRightChild()) n.parent.right=p;
+		else root = p;
+		p.left.parent = p;
+		p.right.parent = p;
+		p.height = n.height;
+		n.owner = null;
+		size--;	
 	}
+
+	/**
+	 * @param n
+	 */
+	private void removeAboveExternal(AVLNode n) {
+		
+	}
+
+
+
+
 
 	/* (non-Javadoc)
 	 * @see examples.OrderedDictionary#closestBefore(java.lang.Comparable)
@@ -177,7 +234,7 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 	}
 	
 	public void print(){
-		print(root,"");
+		prittyPrint(root,"");
 	}
 	
 	/**
@@ -190,20 +247,48 @@ public class MyAVLTree<K extends Comparable<? super K>, E> implements
 		System.out.println(ind+r.key);
 		print(r.left,ind+"..");
 	}
+	
+	
+	private void prittyPrint(AVLNode r, String in) {
+		if (r.isExternal()) return;		
+		// right subtree 
+		int sLen = in.length();
+		String inNeu = in;
+		if (r.isRightChild()) inNeu = in.substring(0,sLen-2)+"  ";
+		prittyPrint(r.right,inNeu+" |");
+		// root of the subtree
+		String inN = in;
+		if (sLen>0) inN = in.substring(0,sLen-1)+"+-";
+		else inN = in+"-"; // root of the tree
+		if ( ! r.right.isExternal()) System.out.println(inNeu+" |");
+		else System.out.println(inNeu);
+		System.out.println(inN+r.key()+":"+r.elem); //+"("+r.height+")"); 
+		// left subtree
+		inNeu = in;
+		if (r.isLeftChild()){
+			inNeu = in.substring(0,sLen-2)+"  ";
+		}//		l = t.closestBefore(21);
+//		System.out.println(l.key()+": "+l.element());
+
+		prittyPrint(r.left,inNeu+" |");
+	}
 
 	
 	
 	public static void main(String[] argv){
 		MyAVLTree<Integer,String> t = new MyAVLTree<>();
-		t.insert(10,null);
-		t.insert(5,null);
-		t.insert(20,null);
-		t.insert(30,null);
-		t.insert(15,null);
-		t.insert(25,null);
-		t.insert(7,null);
+		t.insert(10,"");
+		t.insert(5,"");
+		t.insert(20,"occ_1");
+		t.insert(30,"");
+		t.insert(25,"");
+		t.insert(20,"occ_2");
+		t.insert(7,"");
 		System.out.println("tree:");
 		t.print();
+		System.out.println();
+		Locator<Integer,String> loc = t.find(20);
+		System.out.println("found: "+loc.key()+":"+loc.element());
 	}
 
 }
